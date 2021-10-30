23daa2059999d62e3e5f58d3521e85e1a904a46c
krylowicz <kacper@krylowicz.pl>
2021-10-30 22:31:09 +0200

    rules & game loop

--- include/GameOfLife.hh
+++ include/GameOfLife.hh
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "cinder/app/App.h"
 #include "xtensor/xtensor.hpp"
 #include "xtensor/xarray.hpp"
@@ -11,11 +13,14 @@ class GameOfLife : public app::App {
     const size_t rows {app::getWindowWidth() / res};
     const size_t cols {app::getWindowHeight() / res};
     xt::xarray<unsigned> state {xt::random::randint<unsigned>({rows, cols}, 0, 2)};
-
+  
   public:
     bool isCellActive(size_t i, size_t j) const;
     void setCellActive(size_t i, size_t j);
     void setCellInactive(size_t i, size_t j);
+
+    unsigned countNeighbours(size_t i, size_t j) const;
+    void next();
 
     void setup() override;
     void draw() override;
--- src/GameOfLife.cc
+++ src/GameOfLife.cc
@@ -14,7 +14,39 @@ void GameOfLife::setCellActive(size_t i, size_t j) {
 }
 
 void GameOfLife::setCellInactive(size_t i, size_t j) {
-  state(i, j) = 1;
+  state(i, j) = 0;
+}
+
+unsigned GameOfLife::countNeighbours(size_t x, size_t y) const {
+  unsigned neighbours {0};
+  for (int i = -1; i < 2; ++i) {
+    for (int j = -1; j < 2; ++j) {
+      size_t row {(x + rows) % rows};
+      size_t col {(y + cols) % cols};
+
+      if (isCellActive(row + i, col + j)) neighbours++;
+    }
+  }
+
+  return neighbours - 1;
+}
+
+void GameOfLife::next() {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      auto neighbours {countNeighbours(i, j)};
+
+      if (isCellActive(i, j) && neighbours == 0 || neighbours == 1) {
+        setCellInactive(i, j);
+      } else if (isCellActive(i, j) && neighbours >= 4) {
+        setCellInactive(i, j);
+      } else if (isCellActive(i, j) && neighbours >= 2 && neighbours <= 3) {
+        setCellActive(i, j);
+      } else if (!isCellActive(i, j) && neighbours == 3) {
+        setCellActive(i, j);
+      }
+    }
+  }
 }
 
 void GameOfLife::setup() {}
@@ -31,4 +63,5 @@ void GameOfLife::draw() {
       }
     }
   }
+  next();
 }
